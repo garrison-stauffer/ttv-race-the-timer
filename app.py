cd4e5653abf3960219402ef7d79adde1350dd264
Garrison Stauffer <gstauffer@toasttab.com>
2021-12-10 21:02:34 -0500

    hack: make font size 100px for easier viewing on OBS

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	return "Hello World!"
+	return "<h1 style=\"font-size:100px\">Hello World!</h1>"
 
 if __name__ == "__main__":
 	app.run(port = 51023)
