e1df2967f93bd7b0fba1c37ce3e1a0ac52c79d50
Garrison Stauffer <gstauffer@toasttab.com>
2021-12-12 20:23:41 -0500

    (very) simple login workflow

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
 	return render_template('index.html')
+
+@app.route("/auth/login")
+def login():
+	return render_template('login.html')
+
+@app.route("/auth/callback")
+def login_callback():
+	return render_template('login_callback.html', data=request.args)
 
 if __name__ == "__main__":
 	app.run(port = 51023)
